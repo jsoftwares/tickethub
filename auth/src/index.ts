fa4e54e6c0598af494762ccaa806b1b2d7c44eb6
Jeffrey Onochie <jeffreyonochie@gmail.com>
2021-02-24 06:07:56 +0100

    initial commit - Setup bioler plate for Auth service,create k8s depl & srv objects

---
+++ auth/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import {json} from 'body-parser';
+
+
+const app = express();
+
+app.use(json());
+
+
+app.listen(3000, () => console.log('Auth service is running on  Port 3000')
+);
