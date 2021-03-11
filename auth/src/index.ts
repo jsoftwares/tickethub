541d171e73b914a1492c43366a32b639baa90f32
Jeffrey Onochie <jeff.ict@gmail.com>
2021-03-11 05:10:52 +0100

    Add password hashing and compare class

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors'
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import {currentUserRouter} from './routes/current-user';
 import {signupRouter} from './routes/signup';
@@ -24,6 +25,18 @@ app.all('*', async (req, res) => {
 });
 app.use(errorHandler);
 
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+        });
+        console.log('Auth connected to MongoDB');
+        
+    } catch (err) {
+        console.error(err);
+    }
 
-app.listen(3000, () => console.log('Auth service is running on  Port 3000')
-);
+    app.listen(3000, () => console.log('Auth service is running on  Port 3000'));
+}
+
+start();
