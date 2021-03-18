f6de5e268bef504cb2b13aa4b38e5722dc9b596c
Jeffrey Onochie <jeff.ict@gmail.com>
2021-03-18 19:22:45 +0100

    Add Signout logic & route authorization middlewares

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,20 +1,12 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
+
+import {currentUser} from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-    if (!req.session?.jwt) {
-        return res.send({currenUser: null});
-    }
-
-    // if d token cannot be verified,JWT will throw an error, so we need to wrap the logic in a try-catch in other to catch the error
-    try {
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-        res.send({currentUser: payload});
-    } catch (error) {
-        res.send({currentUser: null});
-    }
+router.get('/api/users/currentuser', currentUser, (req, res) => {
+    
+    res.status(200).send({currentUser: req.currentUser || null});
 });
 
-export {router as currentUserRouter}; 
+export {router as currentUserRouter};
--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -2,8 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.post('/api/users/signout',
-    (req, res) => {
+router.post('/api/users/signout', (req, res) => {
         /**To signout a user, we simply send back a header that will tell d user's browser to dump all the 
          * information inside that cookie, which remove the JWT hence when the user makes a followup 
          * request there will be no token included inside that cookie. We will use cookie session to destroy
